d12c9d40e4a06d2162ca71145decd47be7d9d9d9
Lucas Fernandez Garabenta <lucasrafaelfg@gmail.com>
2021-11-15 19:36:44 -0300

    First Commit

---
+++ frontend/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from django.core import validators
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+from . import models
+
+# Create your forms here
